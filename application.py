62d08aa560d512bc6fbff740e1a214476d521336
Alexander Leighton <a.leighton@protonmail.com>
2018-02-27 15:25:10 +0800

    Add message flashing to app

--- application.py
+++ application.py
@@ -21,6 +21,8 @@ def categoryItems(category_id):
     category = session.query(Category).filter_by(id = category_id).one()
     items = session.query(Item).filter_by(category_id = category_id)
     return render_template('category.html', category=category, items=items)
+
+
 # 1.Write create new item function
 @app.route('/catalog/<int:category_id>/new/', methods=['GET', 'POST'])
 def newCategoryItem(category_id):
@@ -31,6 +33,7 @@ def newCategoryItem(category_id):
                        category_id = category_id)
         session.add(newItem)
         session.commit()
+        flash("%s has been created!" % newItem.name)
         return redirect(url_for('categoryItems', category_id = category_id))
     else:
         return render_template('newItem.html', category_id = category_id)
@@ -49,6 +52,7 @@ def editCategoryItem(category_id, item_id):
             editedItem.price = request.form['price']
         session.add(editedItem)
         session.commit()
+        flash("%s has been edited" % editedItem.name)
         return redirect(url_for('categoryItems', category_id=category_id))
     else:
         return render_template('editItem.html',
@@ -58,10 +62,18 @@ def editCategoryItem(category_id, item_id):
 
 
 # 3.Write delete item function
-@app.route('/catalog/<int:category_id>/<int:item_id>/delete/')
+@app.route('/catalog/<int:category_id>/<int:item_id>/delete/', methods=['GET', 'POST'])
 def deleteCategoryItem(category_id, item_id):
-    return "A page to delete an item"
+    itemToDelete = session.query(Item).filter_by(id=item_id).one()
+    if request.method == 'POST':
+        session.delete(itemToDelete)
+        session.commit()
+        flash("%s has been deleted!" % itemToDelete)
+        return redirect(url_for('categoryItems', category_id=category_id))
+    else:
+        return render_template('deleteItem.html', category_id=category_id, i=itemToDelete)
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
